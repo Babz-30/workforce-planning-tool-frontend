f50d1ce9d94a5e03dfa418a0eba540057ca51b2a
Babitha_30 <babitha.nadar.eu@gmail.com>
2025-12-10 18:47:58 +0100

    Fix to use mock settings and backend url from .env file

--- src/services/mock/apiMockLogin.js
+++ src/services/mock/apiMockLogin.js
@@ -6,11 +6,11 @@ export const login = async (username, password) => {
     // Define mock users with full response data
     const mockUsers = [
         {
-            username: "emp_alice",
+            username: "emp_alicee",
             password: "emp123",
             data: {
                 "_id": { "$oid": "6910fe3647a984367dd713e0" },
-                "username": "emp_alice",
+                "username": "emp_alicee",
                 "firstName": "Alice",
                 "lastName": "Williams",
                 "email": "alice.williams@workforce.com",
